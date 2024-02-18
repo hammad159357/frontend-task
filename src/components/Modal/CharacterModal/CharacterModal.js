e4595343060eb5c239a1e46f9654eaeba11d637b
Muhammad Hammad <hammadahmad159357@gmail.com>
2024-02-18 23:16:24 +0500

    Add and Edit functionality

--- src/components/Modal/CharacterModal/CharacterModal.js
+++ src/components/Modal/CharacterModal/CharacterModal.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Modal, Spinner } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 import { parseDate } from '../../../utils/functions';
+import Loader from '../../Loader/Loader';
 
 const CharacterModal = ({ isOpen, onClose, character, homeworld, isLoading }) => {
     if (!isOpen) return null;
-    const { height, mass, birth_year, films, created } = character
-    const { name, climate, residents, terrain } = homeworld
-    const date = parseDate(created)
+    const { height, mass, birth_year, films, created, characterFilms, name, climate, residents, terrain } = character
 
     return (
         <>
@@ -18,15 +17,7 @@ const CharacterModal = ({ isOpen, onClose, character, homeworld, isLoading }) =>
 
                 <Modal.Body className='position-relative'>
                     {isLoading &&
-                        <div className="text-center position-absolute top-0 start-0" 
-                        style={{
-                            backgroundColor: "rgb(135 135 133 / 50%)",
-                            height: "500px", width: "500px"
-                        }}>
-                            <div className='d-flex justify-content-center align-items-center' style={{ height: "500px", width: "500px" }}>
-                                {<Spinner animation="border" variant="primary" />}
-                            </div>
-                        </div>}
+                        <Loader />}
 
                     <div className="mb-4">
                         <div className='text-center mb-3'>
@@ -34,15 +25,15 @@ const CharacterModal = ({ isOpen, onClose, character, homeworld, isLoading }) =>
                         </div>
                         <div className="border px-3">
                             <div className="row py-3">
-                                <div className="col"><span  className='fw-bold'>Height:</span> {height} cm</div>
+                                <div className="col"><span className='fw-bold'>Height:</span> {height} cm</div>
                                 <div className="col"><span className='fw-bold'>Mass:</span> {mass} kg</div>
                             </div>
                             <div className="row py-3">
                                 <div className="col"><span className='fw-bold'>Birth Year:</span> {birth_year}</div>
-                                <div className="col"><span className='fw-bold'>Number of Films:</span> {films?.length}</div>
+                                <div className="col"><span className='fw-bold'>Number of Films:</span> {characterFilms ?? films?.length}</div>
                             </div>
                             <div className="row py-3">
-                                <div className="col-12"><span className='fw-bold'>Date Added to Api:</span> {date}</div>
+                                <div className="col-12"><span className='fw-bold'>Date Added to Api:</span> {parseDate(created)}</div>
                             </div>
                         </div>
                     </div>
@@ -52,14 +43,14 @@ const CharacterModal = ({ isOpen, onClose, character, homeworld, isLoading }) =>
                         </div>
                         <div className="border px-3">
                             <div className="row py-3">
-                                <div className="col"><span className='fw-bold'>Name:</span> {name} </div>
-                                <div className="col"><span className='fw-bold'>Climate:</span> {climate} </div>
+                                <div className="col"><span className='fw-bold'>Name:</span> {name ?? homeworld?.name} </div>
+                                <div className="col"><span className='fw-bold'>Climate:</span> {climate ?? homeworld?.climate} </div>
                             </div>
                             <div className="row py-3">
-                                <div className="col-12"><span className='fw-bold'>Number of Residents:</span> {residents?.length}</div>
+                                <div className="col-12"><span className='fw-bold'>Number of Residents:</span> {residents ?? homeworld?.residents?.length}</div>
                             </div>
                             <div className="row py-3">
-                                <div className="col-12"><span className='fw-bold'>Terrain:</span> {terrain}</div>
+                                <div className="col-12"><span className='fw-bold'>Terrain:</span> {terrain ?? homeworld?.terrain}</div>
                             </div>
                         </div>
                     </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Spinner, Button } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Card from '../components/Card/Card'
 import Header from '../components/Layout/Header/Header'
 import CharacterModal from '../components/Modal/CharacterModal/CharacterModal'
-import { deleteRequest, getRequest } from '../utils/api'
+import { deleteRequest, getRequest, postRequest, putRequest } from '../utils/api'
 import { AppContext } from '../App'
 import { notifyError, notifySuccess } from '../utils/functions'
+import CharacterDetailsModal from '../components/Modal/CharacterDetailsModal/CharacterDetailsModal';
 import './style.css'
 
 const LandingPage = () => {
@@ -15,19 +18,18 @@ const LandingPage = () => {
     const [modalLoader, setModalLoader] = useState(false);
     const [page, setPage] = useState(1);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedCharacter, setSelectedCharacter] = useState(null);
+    const [addModalOpen, setAddModalOpen] = useState(false);
+    const [selectedCharacter, setSelectedCharacter] = useState({});
     const [homeWorld, setHomeWorld] = useState([]);
-
+    const [validated, setValidated] = useState(false);
 
     useEffect(() => {
-        getData();
+        fetchCharactersData();
     }, []);
 
-    const getData = async (page = 1) => {
-        if (appData?.pages && appData?.pages[page]) {
-            setData(appData?.pages[page])
-            return
-        }
+    //Fetch starwars data
+    const fetchCharactersData = async (page = 1) => {
+        setPage(page)
         setIsLoading(true)
         try {
             const response = await getRequest(`http://localhost:3000/api/starWars/people?page=${page}`);
@@ -38,9 +40,18 @@ const LandingPage = () => {
             notifyError(error, "top-left")
             setIsLoading(false);
         }
-
     }
 
+    //If data exist in context api hook then manage from it and if not then call star wars api
+    const getPaginationData = async (page = 1) => {
+        if (appData?.pages && appData?.pages[page]) {
+            setData(appData?.pages[page])
+            return
+        }
+        fetchCharactersData(page)
+    }
+
+    //Get character homeworld info data
     const getHomeWorldData = async (url) => {
         if (appData?.homeworld && appData?.homeworld[url]) {
             setHomeWorld(appData?.homeworld[url])
@@ -60,67 +71,157 @@ const LandingPage = () => {
 
     const handleNextPage = () => {
         setPage(page + 1);
-        getData(page + 1)
+        getPaginationData(page + 1)
     };
 
     const handlePrevPage = () => {
         if (page > 1) {
             setPage(page - 1);
-            getData(page - 1)
+            getPaginationData(page - 1)
         }
     }
 
-    const openCharacterModal = (id,character) => {
+    //Open Character Modal
+    const openCharacterModal = (id, character) => {
         setSelectedCharacter(character)
         getHomeWorldData(character?.homeworld)
         setIsModalOpen(true);
     };
-   
 
     const closeModal = () => {
-        setSelectedCharacter(null)
+        setSelectedCharacter({})
+        setHomeWorld([])
         setIsModalOpen(false);
+        setAddModalOpen(false);
     };
 
-    const onCardDelete = (id, element) => {
-        const filterData = appData?.pages[page]?.filter((item, index) => id !== index)
-        appData.pages[page] = filterData
-        setData(appData?.pages[page])
-        deleteRequest(`http://localhost:3000/api/starWars/people/delete`, { id })
+    //It deletes character from frontent in context api hook and call delete api to delete character from backend
+    const onCardDelete = async (id, element) => {
+        try {
+            await deleteRequest(`http://localhost:3000/api/starWars/people/${id}`);
+            const filterData = appData?.pages[page]?.filter((item) => item?._id !== id)
+            const updatedData = {
+                ...appData,
+                pages: { ...appData.pages, [page]: filterData }
+            }
+            setAppData(updatedData);
+            setData(updatedData?.pages[page])
+            notifySuccess("Character deleted Successfully");
+        } catch (error) {
+            console.error('API request failed:', error);
+            notifyError("Failed to delete character. Please try again later.");
+        }
     }
+
+    const onCardEdit = (id, character) => {
+        setSelectedCharacter(character)
+        character?.homeworld && getHomeWorldData(character?.homeworld)
+        setAddModalOpen(true)
+    }
+
+    const openAddModal = () => {
+        setAddModalOpen(true)
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setSelectedCharacter({ ...selectedCharacter, [name]: value });
+    };
+
+    //Handle Character Details input 
+    const handleSubmit = async (event) => {
+        const form = event.currentTarget;
+        event.preventDefault();
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+            setValidated(true);
+            return
+        }
+        try {
+            //Edit and Update Character Details
+            if (selectedCharacter?._id) {
+                await putRequest(`http://localhost:3000/api/starWars/people/${selectedCharacter?._id}`, { ...selectedCharacter })
+                let updatedData = {
+                    ...appData,
+                    pages: {
+                        ...appData.pages, [page]: appData.pages[page]?.map(s => {
+                            if (s._id === selectedCharacter?._id) {
+                                return {
+                                    ...s, ...selectedCharacter
+                                }
+                            }
+                            return s
+                        })
+                    }
+                }
+                setAppData(updatedData);
+                setData(updatedData?.pages[page])
+                notifySuccess("Character updated Successfully");
+            }
+            //Add New Character 
+            else {
+                await postRequest(`http://localhost:3000/api/starWars/people`, { ...selectedCharacter })
+                fetchCharactersData()
+                notifySuccess("Character added Successfully");
+            }
+            setValidated(false);
+            closeModal()
+        } catch (err) {
+            console.error('API request failed:', err);
+            notifyError("Failed to update character. Please try again later.");
+        }
+    };
 
     return (
         <>
-            <Header />
-            {isLoading ?
-                <div className="Container pagination">
-                    <Spinner animation="border" variant="primary" />
-                </div>
-                :
-                <>
+            <div>
+                <Header />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    closeOnClick
+                    rtl={false}
+                    draggable
+                />
+                {isLoading ?
                     <div className="Container pagination">
-                        <button onClick={handlePrevPage} disabled={page === 1}>Previous</button>
-                        <span>Page {page}</span>
-                        <button onClick={handleNextPage}>Next</button>
+                        <Spinner animation="border" variant="primary" />
                     </div>
-                    <div className="Container">
-
-
-                        {data?.map((element, index) => {
-                            const { name, skin_color, image } = element
-                            return (
-                                <div key={index}>
-                                    <Button onClick={() => onCardDelete(index, element)} style={{ marginBottom: "10px" }} variant="danger">Delete</Button>{' '}
-                                    <div key={index} onClick={() => openCharacterModal(index,element)} >
-                                        <Card key={index} name={name} skinColor={skin_color} image={image} />
+                    :
+                    <>
+                        <div className="Container pagination">
+                            <button onClick={handlePrevPage} disabled={page === 1}>Previous</button>
+                            <span>Page {page}</span>
+                            <button onClick={handleNextPage}>Next</button>
+                            <div>
+                                <Button onClick={openAddModal} variant="primary">Add</Button>
+                            </div>
+                        </div>
+                        <div className="Container">
+                            {data?.length > 0 ? data?.map((element, index) => {
+                                const { _id, name, skin_color, image } = element
+                                return (
+                                    <div key={index}>
+                                        <div key={index} onClick={() => openCharacterModal(index, element)} >
+                                            <Card key={index} name={name} skinColor={skin_color} image={image} />
+                                        </div>
+                                        <div style={{ display: "flex", justifyContent: "center", gap: '20px', marginTop: '15px' }}>
+                                            <Button onClick={() => onCardDelete(_id, element)} style={{ marginBottom: "10px" }} variant="danger">Delete</Button>
+                                            <Button onClick={() => onCardEdit(_id, element)} style={{ marginBottom: "10px" }} variant="primary">Edit</Button>
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </>
-            }
-            <CharacterModal isLoading={modalLoader} isOpen={isModalOpen} onClose={closeModal} character={selectedCharacter} homeworld={homeWorld} />
+                                )
+                            }) : "No Data Found"}
+                        </div>
+                    </>
+                }
+                <CharacterDetailsModal isLoading={modalLoader} homeworld={homeWorld} isOpen={addModalOpen} onClose={closeModal}
+                    handleSubmit={handleSubmit} handleChange={handleChange} character={selectedCharacter} validated={validated}
+                />
+                <CharacterModal isLoading={modalLoader} isOpen={isModalOpen} onClose={closeModal}
+                    character={selectedCharacter} homeworld={homeWorld} />
+            </div>
         </>
     )
 }
